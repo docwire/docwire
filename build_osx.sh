ea5b9a719630a683bce52c1389318da1fc445ddb
as-ascii <62467072+as-ascii@users.noreply.github.com>
2023-06-20 21:53:54 +0200

    Fix OSX building process

--- build_osx.sh
+++ build_osx.sh
@@ -123,6 +123,7 @@ wget https://github.com/do-m-en/libarchive_cpp_wrapper/archive/refs/tags/1.0.0.t
 tar -xzvf 1.0.0.tar.gz
 cd libarchive_cpp_wrapper-1.0.0
 cp archive*.hpp archive*.ipp /usr/local/include/
+sed -i 's,linux/types.h,cstdint,' /usr/local/include/archive_entry.hpp
 cat archive*.cpp > /usr/local/include/libarchive_cpp_wrapper.hpp
 cd ..
 rm -rf libarchive_cpp_wrapper
