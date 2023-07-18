cf18da0259ca2d585bd62c1950624bbf8b866b51
as-ascii <62467072+as-ascii@users.noreply.github.com>
2023-07-18 02:59:19 +0200

    Fix building unzip in build.ps1

--- build.ps1
+++ build.ps1
@@ -114,7 +114,7 @@ Write-Output 'cmake_minimum_required(VERSION 3.16)' `
       > CMakeLists.txt
 mkdir build
 cd build
-cmake .. -DCMAKE_TOOLCHAIN_FILE="$vcpkg_toolchain" -DCMAKE_PREFIX_PATH="$vcpkg_prefix" -DCMAKE_INSTALL_PREFIX:PATH="$deps_prefix"
+cmake .. -DCMAKE_TOOLCHAIN_FILE="$vcpkg_toolchain" -DCMAKE_CXX_FLAGS="-I$vcpkg_prefix/include" -DCMAKE_INSTALL_PREFIX:PATH="$deps_prefix"
 cmake --build . --config Release
 cmake --build . --config Release --target install
 cd ..
