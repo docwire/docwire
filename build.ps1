dce54e24433535ad424d31cad4bf9a361dcd0310
as-ascii <62467072+as-ascii@users.noreply.github.com>
2023-08-28 12:09:18 +0200

    Fix creating temporary source archive in MSVC build

--- build.ps1
+++ build.ps1
@@ -22,7 +22,7 @@ $exclude = @("vcpkg", "doctotext*", ".git", "sources-temp.tar")
 Write-Host "before PWD"
 Write-Host "PWD: $PWD"
 Write-Host "after PWD"
-$files = Get-ChildItem -Exclude $exclude
+$files = Get-ChildItem -Path * -Exclude $exclude
 Write-Host "files="
 $files | Write-Output
 $files_no_exclude = Get-ChildItem
