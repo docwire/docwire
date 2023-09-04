63cf495f83a92e4346554987d752e719b883e163
as-ascii <62467072+as-ascii@users.noreply.github.com>
2023-09-04 17:36:28 +0200

    Add CLI wrapper scripts in root directory of binary archive

--- build.sh
+++ build.sh
@@ -31,5 +31,9 @@ SOURCES_ARCHIVE=$PWD/sources-temp.tar VCPKG_KEEP_ENV_VARS=SOURCES_ARCHIVE ./vcpk
 version=`cat ./vcpkg/installed/$VCPKG_TRIPLET/share/doctotext/VERSION`
 ./vcpkg/vcpkg --overlay-ports=./ports export doctotext:$VCPKG_TRIPLET --raw --output=doctotext-$version --output-dir=.
 
+echo "\$(dirname \"\$0\")/installed/$VCPKG_TRIPLET/tools/text_extractor.sh \"\$@\"" > doctotext-$version/text_extractor.sh
+echo "\$(dirname \"\$0\")/installed/$VCPKG_TRIPLET/tools/c_text_extractor.sh \"\$@\"" > doctotext-$version/c_text_extractor.sh
+chmod u+x doctotext-$version/text_extractor.sh doctotext-$version/c_text_extractor.sh
+
 tar -cjvf doctotext-$version-$VCPKG_TRIPLET.tar.bz2 doctotext-$version
 sha1sum doctotext-$version-$VCPKG_TRIPLET.tar.bz2 > doctotext-$version-$VCPKG_TRIPLET.tar.bz2.sha1
