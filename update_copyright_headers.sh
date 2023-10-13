6f6374d5ad9c579efcc84f878b7ae3e073e12c36
as-ascii <62467072+as-ascii@users.noreply.github.com>
2023-10-13 19:54:58 +0200

    Remove docker dependency from update_copyright_headers.sh

--- update_copyright_headers.sh
+++ update_copyright_headers.sh
@@ -1,1 +1,1 @@
-docker run --rm --user=$UID -v `pwd`:`pwd` -w `pwd` doctotext_build_env:latest bash -c "headache -c copyright_header.cfg -h copyright_header.txt src/*.{cpp,c,h} tests/*.cpp"
+headache -c copyright_header.cfg -h copyright_header.txt src/*.{cpp,h} tests/*.cpp
