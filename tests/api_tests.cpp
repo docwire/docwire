a0dab91808d57ef95b71ce18ce2819fdf2b79a60
as-ascii <62467072+as-ascii@users.noreply.github.com>
2023-11-02 19:10:29 +0100

    Fix automatic test for logging dereferenceables - replace ::TestBody(void) with ::TestBody() in function signature

--- tests/api_tests.cpp
+++ tests/api_tests.cpp
@@ -688,6 +688,7 @@ TEST(Logging, Dereferenceable)
 	boost::algorithm::erase_all(log_text, "__cdecl ");
 	boost::algorithm::erase_all(log_text, "__1::");
 	boost::algorithm::erase_all(log_text, "virtual ");
+	boost::algorithm::replace_all(log_text, "::TestBody(void)", "::TestBody()");
 
 	ASSERT_EQ(read_test_file("logging_dereferenceable.out.json"), log_text);
 }
