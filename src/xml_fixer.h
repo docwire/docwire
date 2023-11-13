4656221af2942be5f86938cb81e19b5a2bdb672e
as-ascii <62467072+as-ascii@users.noreply.github.com>
2023-11-13 19:42:36 +0100

    Update copyright headers, introduce SPDX license identifiers

--- src/xml_fixer.h
+++ src/xml_fixer.h
@@ -1,35 +1,13 @@
-/***************************************************************************************************************************************************/
-/*  DocWire SDK - A multifaceted, data extraction software development toolkit that converts all sorts of files to plain text and html.            */
-/*  Written in C++, this data extraction tool has a parser able to convert PST & OST files along with a brand new API for better file processing.  */
-/*  To enhance its utility, DocWire, as a data extraction tool, can be integrated with other data mining and data analytics applications.          */
-/*  It comes equipped with a high grade, scriptable and trainable OCR that has LSTM neural networks based character recognition.                   */
-/*                                                                                                                                                 */
-/*  This document parser is able to extract metadata along with annotations and supports a list of formats that include:                           */
-/*  DOC, XLS, XLSB, PPT, RTF, ODF (ODT, ODS, ODP), OOXML (DOCX, XLSX, PPTX), iWork (PAGES, NUMBERS, KEYNOTE), ODFXML (FODP, FODS, FODT),           */
-/*  PDF, EML, HTML, Outlook (PST, OST), Image (JPG, JPEG, JFIF, BMP, PNM, PNG, TIFF, WEBP), Archives (ZIP, TAR, RAR, GZ, BZ2, XZ)                  */
-/*  and DICOM (DCM)                                                                                                                                */
-/*                                                                                                                                                 */
-/*  Copyright (c) SILVERCODERS Ltd                                                                                                                 */
-/*  http://silvercoders.com                                                                                                                        */
-/*                                                                                                                                                 */
-/*  Project homepage:                                                                                                                              */
-/*  https://github.com/docwire/docwire                                                                                                             */
-/*  https://www.docwire.io/                                                                                                                        */
-/*                                                                                                                                                 */
-/*  The GNU General Public License version 2 as published by the Free Software Foundation and found in the file COPYING.GPL permits                */
-/*  the distribution and/or modification of this application.                                                                                      */
-/*                                                                                                                                                 */
-/*  Please keep in mind that any attempt to circumvent the terms of the GNU General Public License by employing wrappers, pipelines,               */
-/*  client/server protocols, etc. is illegal. You must purchase a commercial license if your program, which is distributed under a license         */
-/*  other than the GNU General Public License version 2, directly or indirectly calls any portion of this code.                                    */
-/*  Simply stop using the product if you disagree with this viewpoint.                                                                             */
-/*                                                                                                                                                 */
-/*  According to the terms of the license provided by SILVERCODERS and included in the file COPYING.COM, licensees in possession of                */
-/*  a current commercial license for this product may use this file.                                                                               */
-/*                                                                                                                                                 */
-/*  This program is provided WITHOUT ANY WARRANTY, not even the implicit warranty of merchantability or fitness for a particular purpose.          */
-/*  It is supplied in the hope that it will be useful.                                                                                             */
-/***************************************************************************************************************************************************/
+/*********************************************************************************************************************************************/
+/*  DocWire SDK: Award-winning modern data processing in C++17/20. SourceForge Community Choice & Microsoft support. AI-driven processing.   */
+/*  Supports nearly 100 data formats, including email boxes and OCR. Boost efficiency in text extraction, web data extraction, data mining,  */
+/*  document analysis. Offline processing possible for security and confidentiality                                                          */
+/*                                                                                                                                           */
+/*  Copyright (c) SILVERCODERS Ltd, http://silvercoders.com                                                                                  */
+/*  Project homepage: https://github.com/docwire/docwire                                                                                     */
+/*                                                                                                                                           */
+/*  SPDX-License-Identifier: GPL-2.0-only OR LicenseRef-DocWire-Commercial                                                                   */
+/*********************************************************************************************************************************************/
 
 #ifndef DOCWIRE_XML_FIXER_H
 #define DOCWIRE_XML_FIXER_H
