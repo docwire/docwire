8a6f302e4eec4f751369d9362424919b46948f45
as-ascii <62467072+as-ascii@users.noreply.github.com>
2023-11-26 19:04:11 +0100

    Fix Upgrading Meson to 1.3.0 on OSX to fix freetype building error

--- build.sh
+++ build.sh
@@ -18,6 +18,9 @@ git checkout tags/2023.11.20
 if [[ "$OSTYPE" == "darwin"* ]]; then
 	sed -i -e 's/0.63/1.3.0/' ports/vcpkg-tool-meson/vcpkg.json
 	sed -i -e 's/0.63.0/1.3.0/' ports/vcpkg-tool-meson/portfile.cmake
+	sed -i -e 's/bb91cea0d66d8d036063dedec1f194d663399cdf/7368795d13081d4928a9ba04d48498ca2442624b/' ports/vcpkg-tool-meson/portfile.cmake
+	sed -i -e 's/e5888eb35dd4ab5fc0a16143cfbb5a7849f6d705e211a80baf0a8b753e2cf877a4587860a79cad129ec5f3474c12a73558ffe66439b1633d80b8044eceaff2da/b2dc940a8859d6b0af8cb762c896d3188cadc1e65e3c7d922d6cb9a4ed7a1be88cd4d51a8aa140319a75e467816e714c409cf74c71c830bbc5f96bb81c1845ce/' ports/vcpkg-tool-meson/portfile.cmake
+	sed -i -e "s/intl_factory/packages['intl'] = intl_factory/" ports/vcpkg-tool-meson/meson-intl.patch
 fi
 ./bootstrap-vcpkg.sh
 cd ..
