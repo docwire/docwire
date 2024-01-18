920d73d520dc0b30372e7765eb2d011a2c8b2b50
as-ascii <62467072+as-ascii@users.noreply.github.com>
2024-01-18 01:42:56 +0100

    Add debug message regarding OPENAI_API_KEY to build.ps1

--- build.ps1
+++ build.ps1
@@ -13,6 +13,13 @@ $VCPKG_TRIPLET="x64-windows"
 Get-Date | Out-File -FilePath ports\docwire\disable_binary_cache.tmp
 $Env:SOURCE_PATH = "$PWD"
 $Env:VCPKG_KEEP_ENV_VARS = "SOURCE_PATH"
+if (Eenv:OPENAI_API_KEY -ne $null -and $env:OPENAI_API_KEY -ne "") {
+    Write-Host "DEBUG: OPENAI_API_KEY exists and is not empty."
+} elseif ($env:OPENAI_API_KEY -eq "") {
+    Write-Host "DEBUG: OPENAI_API_KEY exists but is empty."
+} else {
+    Write-Host "DEBUG: OPENAI_API_KEY does not exist."
+}
 vcpkg\vcpkg --overlay-ports=ports install docwire[tests]:$VCPKG_TRIPLET
 
 $version = Get-Content vcpkg\installed\$VCPKG_TRIPLET\share\docwire\VERSION
