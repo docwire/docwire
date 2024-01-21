d5a7193d3dc33060488716185b59e1cdf0e4ec5f
as-ascii <62467072+as-ascii@users.noreply.github.com>
2024-01-21 16:52:57 +0100

    Enable Address and Thread sanitizers on all platforms

--- build.ps1
+++ build.ps1
@@ -10,6 +10,19 @@ cd ..
 
 $VCPKG_TRIPLET="x64-windows"
 
+if ($Env:SANITIZER -eq "address")
+{
+	$FEATURES = "[tests,address-sanitizer]"
+}
+elseif ($Env:SANITIZER -eq "thread")
+{
+	$FEATURES = "[tests,thread-sanitizer]"
+}
+else
+{
+	$FEATURES = "[tests]"
+}
+
 Get-Date | Out-File -FilePath ports\docwire\disable_binary_cache.tmp
 $Env:SOURCE_PATH = "$PWD"
 $Env:VCPKG_KEEP_ENV_VARS = "SOURCE_PATH;OPENAI_API_KEY"
@@ -20,7 +33,7 @@ if ($Env:OPENAI_API_KEY -ne $null -and $env:OPENAI_API_KEY -ne "") {
 } else {
     Write-Host "DEBUG: OPENAI_API_KEY does not exist."
 }
-vcpkg\vcpkg --overlay-ports=ports install docwire[tests]:$VCPKG_TRIPLET
+vcpkg\vcpkg --overlay-ports=ports install docwire$FEATURES:$VCPKG_TRIPLET
 
 $version = Get-Content vcpkg\installed\$VCPKG_TRIPLET\share\docwire\VERSION
 vcpkg\vcpkg --overlay-ports=ports export docwire:$VCPKG_TRIPLET --raw --output=docwire-$version --output-dir=.
