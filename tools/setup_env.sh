361c14385eff0ed53efe2db13a07e6644ea6df35
as-ascii <62467072+as-ascii@users.noreply.github.com>
2024-02-14 02:17:24 +0100

    Add script location debug message in build.sh on MacOSX

--- tools/setup_env.sh
+++ tools/setup_env.sh
@@ -5,6 +5,7 @@ if [[ -z "$1" ]]; then
         return 1 || exit 1
     fi
     if [[ "$OSTYPE" == "darwin"* ]]; then
+        echo "script location: $( dirname "$0" )"
         installed_dir="$( cd "$( dirname "$0" )" && cd "installed/$vcpkg_triplet" && pwd )" # zsh version
     else
         installed_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd "installed/$vcpkg_triplet" && pwd )" # bash version
