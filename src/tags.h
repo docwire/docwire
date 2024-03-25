c4faffca3eac85f4ec0e5257ddbf7f9034487153
as-ascii <62467072+as-ascii@users.noreply.github.com>
2024-03-25 02:54:03 +0100

    Add explicit deduction guide for "overload" to fix build error on MacOSX

--- src/tags.h
+++ src/tags.h
@@ -27,6 +27,7 @@ namespace docwire
 
 template<class... Ts>
 struct overloaded : Ts... { using Ts::operator()...; };
+template<class... Ts> overloaded(Ts...) -> overloaded<Ts...>;
 
 namespace attributes
 {
