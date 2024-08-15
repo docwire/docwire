8337049ff0bd117876e752652e3470c950d64b24
as-ascii <62467072+as-ascii@users.noreply.github.com>
2024-08-15 20:59:56 +0200

    Add missing linking of wv2 to docwire_rtf library. It is used for charcode conversion (should be replaced in the future)

--- src/rtf.cmake
+++ src/rtf.cmake
@@ -1,8 +1,9 @@
 add_library(docwire_rtf SHARED rtf_parser.cpp)
 
-target_link_libraries(docwire_rtf PRIVATE docwire_core)
 find_path(wv2_incdir wv2/ustring.h) # because of misc.h, TODO: misc.h should be splitted
 target_include_directories(docwire_rtf PRIVATE ${wv2_incdir})
+find_library(wv2 wv2 HINTS ${wv2_incdir}/../lib/static REQUIRED)
+target_link_libraries(docwire_rtf PRIVATE ${wv2} docwire_core)
 
 install(TARGETS docwire_rtf)
 if(MSVC)
