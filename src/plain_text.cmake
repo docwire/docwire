1449194f3f4548db3d97524e579ab3fb75f9056b
as-ascii <62467072+as-ascii@users.noreply.github.com>
2024-08-15 20:17:52 +0200

    Add linking htmlcxx to docwire_plain_text because of charset converstion routines

--- src/plain_text.cmake
+++ src/plain_text.cmake
@@ -1,7 +1,8 @@
 add_library(docwire_plain_text SHARED txt_parser.cpp)
 
 find_library(charsetdetect charsetdetect REQUIRED)
-target_link_libraries(docwire_plain_text PRIVATE ${charsetdetect} docwire_core)
+find_library(htmlcxx htmlcxx REQUIRED) # for charset conversion
+target_link_libraries(docwire_plain_text PRIVATE ${charsetdetect} ${htmlcxx} docwire_core)
 find_path(charsetdetect_inc_dir charsetdetect.h REQUIRED)
 target_include_directories(docwire_plain_text PRIVATE ${charsetdetect_inc_dir})
 
