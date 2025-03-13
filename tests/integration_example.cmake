fae3c0aebe772f78d207fdcff308d252bd05b7bb
as-ascii <62467072+as-ascii@users.noreply.github.com>
2025-03-13 17:58:16 +0100

    Move copying of libmagic resource file to integration_example.cmake and add flan-t5 and tesseract resources copying

--- tests/integration_example.cmake
+++ tests/integration_example.cmake
@@ -17,3 +17,10 @@ add_executable(integration_example integration_example.cpp)
 find_package(docwire CONFIG REQUIRED)
 # Choose SDK libraries that you need to use
 target_link_libraries(integration_example PRIVATE docwire_core docwire_content_type docwire_office_formats docwire_mail)
+
+if (WIN32 AND VCPKG_APPLOCAL_DEPS)
+	# DLL files are copied to application directory on Windows (see VCPKG_APPLOCAL_DEPS) but resources are not
+    file(COPY ${VCPKG_INSTALLED_DIR}/${VCPKG_TARGET_TRIPLET}/share/libmagic/misc/magic.mgc DESTINATION ${CMAKE_CURRENT_BINARY_DIR}/share/libmagic/misc)
+    file(COPY ${VCPKG_INSTALLED_DIR}/${VCPKG_TARGET_TRIPLET}/share/flan-t5-large-ct2-int8 DESTINATION ${CMAKE_CURRENT_BINARY_DIR}/share)
+	file(COPY ${VCPKG_INSTALLED_DIR}/${VCPKG_TARGET_TRIPLET}/share/tessdata-fast DESTINATION ${CMAKE_CURRENT_BINARY_DIR}/share)
+endif()
