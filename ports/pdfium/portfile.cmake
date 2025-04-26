be067ba016f399b3a5fb78eea8a43befe73a1f94
as-ascii <62467072+as-ascii@users.noreply.github.com>
2025-04-26 04:09:26 +0200

    Fix pdfium building: fix TRAP_SEQUENCE for MSVC

--- ports/pdfium/portfile.cmake
+++ ports/pdfium/portfile.cmake
@@ -2,6 +2,7 @@ vcpkg_from_git(
     OUT_SOURCE_PATH SOURCE_PATH
     URL https://pdfium.googlesource.com/pdfium.git
     REF c1efd963d2f714b1b63ba30ab4d01150fbe1d5d8
+	PATCHES msvc-build.patch
 )
 
 vcpkg_from_git(
