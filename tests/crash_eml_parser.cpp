3c6f467c2e75b39a78e5d3e129f76f1afe515ebe
reeshabh90 <reeshabh.007@gmail.com>
2026-06-30 09:32:48 +0000

    Adding Sample regression eml files for mailio issue check

--- tests/crash_eml_parser.cpp
+++ tests/crash_eml_parser.cpp
@@ -1,21 +1,29 @@
 #include "docwire.h"
+#include <filesystem>
 #include <iostream>
 #include <sstream>
 
-int main(int argc, char* argv[])
-{
+namespace fs = std::filesystem;
+int main(int argc, char *argv[]) {
   using namespace docwire;
-  std::stringstream out_stream;
 
-  try
-  {
-    std::filesystem::path("crash_test_endboundary_first.eml") | content_type::detector{} 
-    | mail_parser{} | plain_text_exporter() | out_stream;
-  }
-  catch (const std::exception& e)
-  {
-    std::cerr << errors::diagnostic_message(e) << std::endl;
+  if (argc < 2) {
+    std::cerr << "No crash eml files supplied\n";
     return 1;
+  }
+
+  for (int i = 1; i < argc; ++i) {
+    fs::path file = argv[i];
+    std::stringstream out_stream;
+    std::cout << "Testing " << file << "\n";
+    try {
+      file | content_type::detector{} | mail_parser{} | plain_text_exporter() |
+          out_stream;
+    } catch (const std::exception &e) {
+      std::cerr << "Exception while parsing " << file << '\n'
+                << errors::diagnostic_message(e) << std::endl;
+      return 1;
+    }
   }
 
   return 0;
