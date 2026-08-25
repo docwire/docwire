4ead6c0d2a5913dc89be615f2da2f6c7242dfbb6
as-ascii <62467072+as-ascii@users.noreply.github.com>
2026-08-25 20:35:27 +0200

    Add missing license headers to source files

--- src/message_counters.h
+++ src/message_counters.h
@@ -1,3 +1,14 @@
+/*********************************************************************************************************************************************/
+/*  DocWire SDK: Award-winning modern data processing in C++20. SourceForge Community Choice & Microsoft support. AI-driven processing.      */
+/*  Supports nearly 100 data formats, including email boxes and OCR. Boost efficiency in text extraction, web data extraction, data mining,  */
+/*  document analysis. Offline processing possible for security and confidentiality                                                          */
+/*                                                                                                                                           */
+/*  Copyright (c) SILVERCODERS Ltd, http://silvercoders.com                                                                                  */
+/*  Project homepage: https://github.com/docwire/docwire                                                                                     */
+/*                                                                                                                                           */
+/*  SPDX-License-Identifier: AGPL-3.0-only OR LicenseRef-DocWire-Commercial                                                                  */
+/*********************************************************************************************************************************************/
+
 #ifndef DOCWIRE_MESSAGE_COUNTERS_H
 #define DOCWIRE_MESSAGE_COUNTERS_H
 
